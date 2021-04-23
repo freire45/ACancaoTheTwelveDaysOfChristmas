428ce5e2082d26e75fa93d3e8c76a20ceb208fe1
Erick Freire <erilefr@gmail.com>
2021-04-23 17:08:06 -0300

    ACancaoTheTwelveDaysOfChristmas
    ACancaoTheTwelveDaysOfChristmas

---
+++ ACancaoTheTwelveDaysOfChristmas/src/br/com/erickfreire/acancaothetwelvedaysofchristimas/ACancaoTheTwelveDaysOfChristmas.java
@@ -0,0 +1,59 @@
+package br.com.erickfreire.acancaothetwelvedaysofchristimas;
+
+
+/**
+ * Programa em Java que exibe A Cancao The Twelve Days Of Christmas
+ * @author Erick Freire
+ * @version 1 - Criado em 23-04-2021 as 16:05
+ */
+
+public class ACancaoTheTwelveDaysOfChristmas {
+
+	public static void main(String[] args) {
+		
+		for(int i = 12; i >= 1; i--) {
+			
+			switch(i) {
+			case 1:
+				System.out.println("The one Days Of Christmas! ");
+				break;
+			case 2:
+				System.out.println("The two Days Of Christmas! ");
+				break;
+			case 3:
+				System.out.println("The three Days Of Christmas! ");
+				break;
+			case 4:
+				System.out.println("The four Days Of Christmas! ");
+				break;
+			case 5:
+				System.out.println("The five Days Of Christmas!");
+				break;
+			case 6:
+				System.out.println("The six Days Of Christmas!");
+				break;
+			case 7:
+				System.out.println("The seven Days Of Christmas! ");
+				break;
+			case 8:
+				System.out.println("The eight Days Of Christmas! ");
+				break;
+			case 9:
+				System.out.println("The nine Days Of Christmas! ");
+				break;
+			case 10:
+				System.out.println("The ten Days Of Christmas! ");
+				break;
+			case 11:
+				System.out.println("The eleven Days Of Christmas!");
+				break;
+			case 12:
+				System.out.println("The twelve Days Of Christmas! ");
+				break;
+			}
+			
+		}
+
+	}
+
+}
